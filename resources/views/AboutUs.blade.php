fba866783a674caee752ffa1e7b67b3a9d7d8fbf
Yumi <ymym2103@gmail.com>
2019-08-27 20:47:35 +0800

    問い合わせ機能作成前

---
+++ resources/views/AboutUs.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <!-- Styles -->
+    <style>
+        .row{
+
+            display: flex;
+            justify-content: center;
+
+        }
+        .vision{
+
+            display: block;
+            font-size: 1.5em;
+            margin-block-start: 0.83em;
+            margin-block-end: 0.83em;
+            margin-inline-start: 0px;
+            margin-inline-end: 0px;
+            font-weight: bold;
+
+        }
+        .mission{
+
+            display: block;
+            font-size: 1.5em;
+            margin-block-start: 0.83em;
+            margin-block-end: 0.83em;
+            margin-inline-start: 0px;
+            margin-inline-end: 0px;
+            font-weight: bold;
+            
+        }
+
+        .about_us{
+            
+            display: flex;
+            justify-content: center;
+            font-size: 1.5em;
+            margin-block-start: 0.83em;
+            margin-block-end: 0.83em;
+            margin-inline-start: 0px;
+            margin-inline-end: 0px;
+            font-weight: bold;
+
+        }
+    </style>
+    <title>About Us</title>
+</head>
+<body>
+    <h1>About Us</h1>
+
+
+
+    <div class="container">
+    　<div class="row">
+        <div class="col-sm-3 p-3">
+            <h2 class="vision">Vision</h2>
+            <p class="vision_content">Make donations/contribution with ZERO price</p>
+        </div>
+        <div class="col-sm-3 p-3 image">
+            <img src="../images/batsu.jpeg">
+        </div>
+        <div class="col-sm-3 p-3">
+            <h3 class="mission">Mission</h3>
+            <p class="mission_content">We will rock you!!</p>
+        </div>
+
+        
+      </div>
+
+      <div>
+        <h4 class="about_us">About us</h4>
+    　</div>
+　　     
+
+    </div>
+
+</body>
+</html>
